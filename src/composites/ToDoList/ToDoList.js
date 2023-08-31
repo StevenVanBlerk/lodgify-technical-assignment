2577065c2bd7bbedebceb629508f5164773fd4b8
Steven van Blerk <stevenvanblerk@offerzen.com>
2023-09-01 00:11:45 +0200

    - Added data fetching of raw group data

--- src/composites/ToDoList/ToDoList.js
+++ src/composites/ToDoList/ToDoList.js
@@ -8,115 +8,32 @@ import {
 import styles from './styles.module.css';
 import { calculateCompletionPercentage, formatGroups } from './helpers';
 
-const groups = [
-  {
-    name: 'General Info',
-    tasks: [
-      {
-        description: 'Add name and surname',
-        value: 10,
-        checked: true,
-      },
-      {
-        description: 'Add email',
-        value: 15,
-        checked: false,
-      },
-      {
-        description: 'Add linkedin profile',
-        value: 8,
-        checked: false,
-      },
-      {
-        description: 'Provide websites page url',
-        value: 5,
-        checked: true,
-      },
-    ],
-  },
-  {
-    name: 'Accomiplishments',
-    tasks: [
-      {
-        description: 'Wrote a small poem about the birthday',
-        value: 23,
-        checked: false,
-      },
-      {
-        description: 'Jump three times with one leg',
-        value: 32,
-        checked: true,
-      },
-      {
-        description: 'Avoid the annoying neighbor',
-        value: 2,
-        checked: false,
-      },
-      {
-        description: 'Say hello to a random person',
-        value: 21,
-        checked: false,
-      },
-      {
-        description: 'Fill the description in at least 3 places',
-        value: 12,
-        checked: true,
-      },
-    ],
-  },
-  {
-    name: 'Personal retrospective',
-    tasks: [
-      {
-        description: 'Remember a dream',
-        value: 15,
-        checked: true,
-      },
-      {
-        description: 'Have a crush',
-        value: 7,
-        checked: true,
-      },
-    ],
-  },
-  {
-    name: 'Things before leaving',
-    tasks: [
-      {
-        description: 'Say what you feel to all the people you know',
-        value: 42,
-        checked: false,
-      },
-      {
-        description: 'Get a pet',
-        value: 23,
-        checked: false,
-      },
-      {
-        description: 'Buy a fashion shirt',
-        value: 12,
-        checked: false,
-      },
-    ],
-  },
-];
-
 const ToDoList = () => {
-  //fetch tasks here
+  const [rawGroupsData, setRawGroupsData] = useState([]);
 
-  const groupsInitialState = formatGroups(groups);
+  const [groupsState, setGroupsState] = useState([]);
+  const [completionPercentage, setCompletionPercentage] = useState(0);
 
-  const [groupsState, setGroupsState] = useState(groupsInitialState);
-  const [completionPercentage, setCompletionPercentage] = useState(
-    calculateCompletionPercentage({ formattedGroups: groupsState, groups })
-  );
+  // add comment here
+  useEffect(() => {
+    fetch(
+      'https://gist.githubusercontent.com/huvber/ba0d534f68e34f1be86d7fe7eff92c96/raw/98a91477905ea518222a6d88dd8b475328a632d3/mock-progress'
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setRawGroupsData(data);
+
+        setGroupsState(formatGroups(data));
+      })
+      .catch((error) => console.error('Error fetching data:', error));
+  }, []);
 
   //add comment here
   useEffect(() => {
     setCompletionPercentage(
       calculateCompletionPercentage({
         formattedGroups: groupsState,
-        groups,
+        groups: rawGroupsData,
       })
     );
   }, [groupsState]);
@@ -137,7 +54,7 @@ const ToDoList = () => {
         <ProgressBar percentageValue={completionPercentage} />
       </div>
       <Accordion>
-        {groups.map((group) => {
+        {rawGroupsData.map((group) => {
           const isGroupComplete =
             Object.values(groupsState[group.name]).filter(
               (isTaskComplete) => !isTaskComplete
